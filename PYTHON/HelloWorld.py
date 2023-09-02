12d5a9db0080fe956b141faa592cce20578b6704
LauraCas06 <115102912+LauraCas06@users.noreply.github.com>
2023-09-02 08:33:15 -0500

    init report
    Evidencias python

---
+++ PYTHON/HelloWorld.py
@@ -0,0 +1,31 @@
+#Esto es un comentario en Python
+#print = imprime el dato 
+#las variables se utilizan directamente
+#no es necesario el ;
+
+print("Hello World")
+
+name = "Pepito"     #Esto es un String
+salary = 10500000   #Esto es un entero
+note = 4.5          #Esto es un Float
+ison= True          #Esto es un Boolean
+b = b"pepito"       #Esto es un Byte
+
+fruits = ["banana","mango", "sandia"]              #Esto es una lista
+names = ("maria", "Luis", "Juan")                  #Esto es Tupla
+cars = {"suv" :"Mazda cx30", "Sedan": "kia rio"}   #Esto es un Diccionario
+ages = {27,36,95,87}                               #Esto es un set
+
+
+#Asi podemos imprimir el contenido de una variable, usando la funcion print
+print(name)
+
+#Para conocer el tipo de dato usamos la funcion type()
+print(type(name))
+print(type(salary))
+print(type(note))
+print(type(ison))
+print(type(fruits))
+print(type(names))
+print(type(cars))
+print(type(ages))
---
+++ PYTHON/Numbers.py
@@ -0,0 +1,16 @@
+num1 = 4.5
+
+#Esto es el casting de variables
+num2 = int(4.5)
+
+number = "3"
+number1 = int("3")
+
+print(number1)
+print(type(number1))
+
+#print(num2)
+
+name = input("Ingrese su nombre: ")
+print("Su Nombre es :" + name)
+
